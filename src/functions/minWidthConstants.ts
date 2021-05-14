8736226447fc7c0329356a7b76038a633a521ede
Tim Welch <tim.j.welch@gmail.com>
2021-05-14 16:58:06 -0700

    add minWidth, shore up clipToOcean

---
+++ src/functions/minWidthConstants.ts
@@ -0,0 +1,4 @@
+/** minimum rectangle width value in miles */
+export const MIN_RECOMMENDED_MIN_WIDTH = 2;
+export const MAX_RECOMMENDED_MIN_WIDTH = undefined;
+export const SKETCH_LENGTH_UNIT = "miles";
---
+++ src/functions/minWidthUnit.test.ts
@@ -0,0 +1,17 @@
+/**
+ * @group unit
+ */
+import Handler from "./minWidth";
+import { getExampleSketchesByName } from "@seasketch/geoprocessing/scripts/testing";
+import * as constants from "./minWidthConstants";
+
+const minWidth = Handler.func;
+
+describe("Unit tests", () => {
+  test(`Minimum width should be at least ${constants.MIN_RECOMMENDED_MIN_WIDTH} ${constants.SKETCH_LENGTH_UNIT}`, async () => {
+    const examples = await getExampleSketchesByName();
+    const result = await minWidth(examples["too-narrow"]);
+    // Verify too-narrow doesn't meet threshold
+    expect(result.minWidth).toBeLessThan(constants.MIN_RECOMMENDED_MIN_WIDTH);
+  });
+});
