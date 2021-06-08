6617a063504c3a5056a5c497964331ef82018aad
Tim Welch <tim.j.welch@gmail.com>
2021-06-08 16:30:44 -0700

    ✨ Add habitat report

---
+++ src/clients/Habitat.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HabitatCard from "./HabitatCard";
+
+const Habitat = () => {
+  return (
+    <>
+      <HabitatCard />
+    </>
+  );
+};
+
+export default Habitat;
