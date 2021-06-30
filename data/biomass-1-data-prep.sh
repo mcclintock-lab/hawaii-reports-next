e9014017d99926c8f486efb299e370409e9ae792
Tim Welch <tim.j.welch@gmail.com>
2021-06-30 11:52:07 -0700

    update biomass data src

--- data/biomass-1-data-prep.sh
+++ data/biomass-1-data-prep.sh
@@ -7,9 +7,9 @@ if [ ! -d src/biomass ]; then
 fi
 
 # Reproject and remove LZW compression
-gdalwarp -t_srs EPSG:4326 src/biomass/browser_biomass_mn.tif dist/browser_biomass_mn.tif
-gdalwarp -t_srs EPSG:4326 src/biomass/grazer_biomass_mn.tif dist/grazer_biomass_mn.tif
-gdalwarp -t_srs EPSG:4326 src/biomass/scraper_biomass_mn.tif dist/scraper_biomass_mn.tif
-gdalwarp -t_srs EPSG:4326 src/biomass/browser_biomass_whi.tif dist/browser_biomass_whi.tif
-gdalwarp -t_srs EPSG:4326 src/biomass/grazer_biomass_whi.tif dist/grazer_biomass_whi.tif
-gdalwarp -t_srs EPSG:4326 src/biomass/scraper_biomass_whi.tif dist/scraper_biomass_whi.tif
+gdalwarp -t_srs EPSG:4326 src/biodiversity/browser_biomass_mn.tif dist/browser_biomass_mn.tif
+gdalwarp -t_srs EPSG:4326 src/biodiversity/grazer_biomass_mn.tif dist/grazer_biomass_mn.tif
+gdalwarp -t_srs EPSG:4326 src/biodiversity/scraper_biomass_mn.tif dist/scraper_biomass_mn.tif
+gdalwarp -t_srs EPSG:4326 src/biodiversity/browser_biomass_whi.tif dist/browser_biomass_whi.tif
+gdalwarp -t_srs EPSG:4326 src/biodiversity/grazer_biomass_whi.tif dist/grazer_biomass_whi.tif
+gdalwarp -t_srs EPSG:4326 src/biodiversity/scraper_biomass_whi.tif dist/scraper_biomass_whi.tif
